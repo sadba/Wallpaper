d29f3397d22abfa81fa7232eb8e499b4a1e13927
sadba <sadba86@gmail.com>
2018-02-23 13:33:38 +0000

    one more adding

--- app/src/main/java/com/lab/sadba/wallpaper2/Common/Common.java
+++ app/src/main/java/com/lab/sadba/wallpaper2/Common/Common.java
@@ -1,8 +1,17 @@
 package com.lab.sadba.wallpaper2.Common;
 
+import com.lab.sadba.wallpaper2.Interface.NewsService;
+import com.lab.sadba.wallpaper2.Remote.RetrofitClient;
+
 /**
  * Created by sadba on 15-Feb-18.
  */
 
 public class Common {
+
+    private static final String BASE_URl="http://13.95.172.201/fgc-admin/C_api_mobile";
+
+    public static NewsService getNewsService(){
+        return RetrofitClient.getClient(BASE_URl).create(NewsService.class);
+    }
 }
